f7be4b0f884640509c5daeec1cb48fb3457f7131
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-06-28 00:13:41 +0200

    Små endringer i kodestil i Utils

--- src/Utils.py
+++ src/Utils.py
@@ -10,17 +10,21 @@ def get_json_path() -> str:
     return get_main_path() + "json/"
 
 
-def get_short_to_long_name(driver_standings):
+def parse_driver_name(driver_name: str) -> str:
+	parsed_driver = ""
+	for c in driver_name[:-3]:
+		if c.isupper():
+			parsed_driver += " "
+		parsed_driver += c
+	return parsed_driver[1:], driver_name[-3:]
+
+
+def get_short_to_long_name(driver_standings: list[list[str]]):
 	short_to_long_name = {}
 	for row in driver_standings[1:]:
-		driver = row[1]
-		driver_split = driver[0]
-		for c in driver[1:-3]:
-			if c.isupper():
-				driver_split += " "
-			driver_split += c
-		driver_shorthand = driver[-3:]
-		short_to_long_name[driver_shorthand] = driver_split
+		driver_name = row[1]
+		driver_name, driver_shorthand = parse_driver_name(driver_name)
+		short_to_long_name[driver_shorthand] = driver_name
 	return short_to_long_name
 
 
@@ -29,7 +33,9 @@ def enough_time_passed_since_race(calendar: list[list[str]], current_race: int):
 	DAYS_BEFORE_SHOWING_RESULTS = 1
 	race_date = calendar[current_race - 1][3].split(".")
 	delta = datetime.datetime.now() - datetime.datetime(
-		int(race_date[2]), int(race_date[1]), int(race_date[0])
+		year = int(race_date[2]),
+		month = int(race_date[1]), 
+		day = int(race_date[0])
 	)
 	enough_time = datetime.timedelta(days=DAYS_BEFORE_SHOWING_RESULTS)
 	return delta > enough_time
