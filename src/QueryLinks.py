db7ec31601b87d7aa131c2d6c20376e090fe9af2
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-06-29 14:39:35 +0200

    Gir verdien til en ny variabel i stedet for å endre på parameter-variabelen

--- src/QueryLinks.py
+++ src/QueryLinks.py
@@ -5,15 +5,15 @@ def __get_race_query_number(race_number: int) -> int:
 
 
 def get_race(race_number: int):
-	race_number = __get_race_query_number(race_number)
+	query_number = __get_race_query_number(race_number)
 	return f'=importhtml("https://www.formula1.com/en/results.html/\
-{Season.get_year()}/races/{race_number}/a/race-result.html"; "table"; 1; "en_US")'
+{Season.get_year()}/races/{query_number}/a/race-result.html"; "table"; 1; "en_US")'
 
 
 def get_start_grid(race_number: int):
-	race_number = __get_race_query_number(race_number)
+	query_number = __get_race_query_number(race_number)
 	return f'=importhtml("https://www.formula1.com/en/results.html/\
-{Season.get_year()}/races/{race_number}/a/starting-grid.html"; "table"; 1; "en_US")'
+{Season.get_year()}/races/{query_number}/a/starting-grid.html"; "table"; 1; "en_US")'
 
 
 def get_driver_standings():
