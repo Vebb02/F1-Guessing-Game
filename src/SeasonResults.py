0bc110622837ee2a54a9efa691acabbeeba45589
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-07-03 22:20:02 +0200

    Alle filene slutter med new line

--- src/SeasonResults.py
+++ src/SeasonResults.py
@@ -8,4 +8,3 @@ class SeasonResults:
 		self.driver_standings: list[list[str]] = driver_standings
 		self.constructor_standings: list[list[str]] = constructor_standings
 		self.race_results: list[list[list[str]]] = race_results
-		
