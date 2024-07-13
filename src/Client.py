fad10d4c6c91d2f8699991a585f7dc313b1c278f
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-07-13 21:49:02 +0200

    Gjort metoder private i CLient

--- src/Client.py
+++ src/Client.py
@@ -6,41 +6,41 @@ from Utils import get_json_path
 
 class Client:
 	def __init__(self):
-		self.client = Client.get_client()
+		self.client = Client.__get_client()
 		self.data: dict[str, str] = None
 
 
-	def load_json(self):
+	def __load_json(self):
 		f = open(get_json_path() + "sheets.json")
 		self.data = json.load(f)
 		f.close()
 	
 
-	def get_id(self, name: str) -> str:
+	def __get_id(self, name: str) -> str:
 		if self.data is None:
-			self.load_json()
+			self.__load_json()
 		return self.data[name]
 
 
-	def get_proxy_id(self) -> str:
-		return self.get_id("proxy")
+	def __get_proxy_id(self) -> str:
+		return self.__get_id("proxy")
 
 
 	def get_proxy_sheet(self):
-		proxy_id = self.get_proxy_id()
+		proxy_id = self.__get_proxy_id()
 		return self.client.open_by_key(proxy_id)
 
 
-	def get_guesses_id(self) -> str:
-		return self.get_id("guesses")
+	def __get_guesses_id(self) -> str:
+		return self.__get_id("guesses")
 
 
 	def get_guesses_sheet(self):
-		guesses_id = self.get_guesses_id()
+		guesses_id = self.__get_guesses_id()
 		return self.client.open_by_key(guesses_id)
 
 
-	def get_client():
+	def __get_client():
 		credentials: service_account.Credentials = (
 			service_account.Credentials.from_service_account_file(
 				get_json_path() + "credentials.json",
