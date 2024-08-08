334647aa47e8ac32fd97ce0a8bb9610aa4b2402d
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-08-08 21:28:05 +0200

    Refaktorering i GuessersList

--- src/GuessersList.py
+++ src/GuessersList.py
@@ -26,23 +26,20 @@ class GuessersList:
 			Cache.cache(rows, GUESSES_FILE)
 		else:
 			rows = rows[0]
+		header = rows[0]
 		for row in rows[1:]:
-			guesser = Guesser(row, rows[0])
-			email = row[len(row) - 1]
+			guesser = Guesser(row, header)
+			email = row[-1]
 			self.email_to_guesser[email] = guesser
 
 	def add_starting_grid(self, list_of_starting_grid: list[list[str]]):
-		for i in range(len(list_of_starting_grid)):
-			starting_grid = list_of_starting_grid[i]
+		for race_number, starting_grid in enumerate(list_of_starting_grid):
 			for guesser in self.list_of_guessers:
-				guesser.add_10th_place_start(i, starting_grid[1:])
+				guesser.add_10th_place_start(race_number, starting_grid[1:])
 
 	def __add_tenth_place_guesses(self, sheet):
 		tenth_place_guessed = get_table_rows(sheet, 1)
-		for row in tenth_place_guessed[1:]:
-			email = row[1]
-			guessed = row[2]
-			race_name = row[3]
+		for _, email, guessed, race_name in tenth_place_guessed[1:]:
 			race_number = int(race_name.split(".")[0]) - 1
 			self.race_number_to_name[race_number] = race_name
 			self.email_to_guesser[email].add_10th_place_guess(race_number, guessed)
@@ -51,10 +48,10 @@ class GuessersList:
 		return self.race_number_to_name
 
 	def add_race_results_to_tenth_place(self, race_results: list[list[str]]):
-		for i in range(len(race_results)):
-			race = race_results[::-1][i]
+		race_results = race_results[::-1]
+		for race_number, race in enumerate(race_results):
 			for guesser in self.list_of_guessers:
-				guesser.add_10th_place_result(i, race[1:])
+				guesser.add_10th_place_result(race_number, race[1:])
 
 	def evaluate_driver_standings(self, driver_standings: list[list[str]]):
 		for guesser in self.list_of_guessers:
@@ -72,7 +69,7 @@ class GuessersList:
 
 	def evaluate_antall(self):
 		self.dict_to_list_of_antall = {}
-		for category_name, stats_key, guesser_key in Stats.get_categories_in_antall():
+		for _, stats_key, guesser_key in Stats.get_categories_in_antall():
 			antall = self.stats.antall[stats_key]
 			antall_list = [
 				(guesser, abs(guesser.antall[guesser_key] - antall)) for guesser in self.list_of_guessers
